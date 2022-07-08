8455c0e769d22f862732cde4173ee89cac385052
Simone Sala <39414823+simonexsala@users.noreply.github.com>
2022-07-08 08:25:11 +0200

    Update weather.sh
    Cleaned

--- polybar/weather.sh
+++ polybar/weather.sh
@@ -1,4 +1,55 @@
 #!/bin/sh
+weatherreport="${XDG_DATA_HOME:-$HOME/.local/share}/weather/weatherreport"
+
+get_forecast() {
+  curl -sf "wttr.in/Milan?format=%C" > "$weatherreport"
+  printf "@" >> "$weatherreport"
+  curl -sf "wttr.in/Milan?format=%t" >> "$weatherreport"
+}
+
+get_data() {
+  weather=$(awk -F'@' '{print $1}' "$weatherreport")
+  temperature=$(awk -F'@' '{print substr($2, 1, length($2)-2)}' "$weatherreport")
+  if [ "${temperature:0:1}" == "+" ] ; then
+    temperature=${temperature:1}
+  fi
+}
+
+print_weather() {
+  display=""
+  now=$(date +"%k%M")
+
+  case $weather in
+    *Sun* | *sun* | *Clear* | *clear*)
+      night_or_day $now
+      ;;
+    *Rain* | *rain* | *showers* | *Sleet* | *sleet*)
+      display+="%{F#04a5e5}歹"
+      ;;
+    *Snow* | *snow*)
+      display+="%{F#89dceb}ﰕ" 
+      ;;
+    *Thunder* | *thunder* | *Wind* | *wind*)
+      display+="%{F#74c7ec}煮"
+      ;;
+    *Fog* | *fog*)
+      display+="%{F#9399b2}敖"
+      ;;
+    *Cloud* | *cloud*)
+      display+="%{F#04a5e5}摒" 
+      ;;
+    "Partly cloudy" | Overcast)
+      display+="%{F#04a5e5}杖" 
+      ;;
+    *)
+      display+="%{F#e78284}" 
+      ;;
+  esac
+
+  display+=" %{F#eff1f5}$temperature"
+  echo "$display"
+}
+
 night_or_day() {
     tempTime=$1
     if [ $tempTime -gt 700 -a $tempTime -lt 2100 ]; then 
@@ -8,46 +59,9 @@ night_or_day() {
     fi
 }
 
-weatherreport="${XDG_DATA_HOME:-$HOME/.local/share}/weather/weatherreport"
-curl -sf "wttr.in/Milan?format=%C" > "$weatherreport"
-printf "@" >> "$weatherreport"
-curl -sf "wttr.in/Milan?format=%t" >> "$weatherreport"
-
-weather=$(awk -F'@' '{print $1}' "$weatherreport")
-temperature=$(awk -F'@' '{print substr($2, 1, length($2)-2)}' "$weatherreport")
-if [ "${temperature:0:1}" == "+" ] ; then
-  temperature=${temperature:1}
+if [ "$(stat -c %y "$weatherreport" 2>/dev/null | cut -d' ' -f 2 | cut -d'.' -f 1)" = "$(date -d '20 minutes ago' '+%T')" ] ; then
+  get_forecast
 fi
 
-display=""
-now=$(date +"%k%M")
-
-case $weather in
-  *Sun* | *sun* | *Clear* | *clear*)
-    night_or_day $now
-    ;;
-  *Rain* | *rain* | *showers* | *Sleet* | *sleet*)
-    display+="%{F#04a5e5}歹"
-    ;;
-  *Snow* | *snow*)
-    display+="%{F#89dceb}ﰕ" 
-    ;;
-  *Thunder* | *thunder* | *Wind* | *wind*)
-    display+="%{F#74c7ec}煮"
-    ;;
-  *Fog* | *fog*)
-    display+="%{F#9399b2}敖"
-    ;;
-  *Cloud* | *cloud*)
-    display+="%{F#04a5e5}摒" 
-    ;;
-  "Partly cloudy" | Overcast)
-    display+="%{F#04a5e5}杖" 
-    ;;
-  *)
-    display+="%{F#e78284}" 
-    ;;
-esac
-
-display+=" %{F#eff1f5}$temperature"
-echo "$display"
+get_data
+print_weather
