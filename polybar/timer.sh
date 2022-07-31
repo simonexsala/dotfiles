efda8e551006c737f6c9f204005d41f75a4e1500
Simone Sala <39414823+simonexsala@users.noreply.github.com>
2022-07-31 23:11:05 +0200

    Update timer.sh

--- polybar/timer.sh
+++ polybar/timer.sh
@@ -4,3 +4,5 @@ start=$(date +%H:%m)
 dunstify "Productivity Timer" "30 minute timer started at $start" -a Timer -t 5000 -u 2
 sleep 1800 # 30 minutes
 dunstify "Productivity Timer" "Time is up!" -a Timer -t 5000 -u 2
+sleep 300 # 5 minutes
+dunstify "Productivity Timer" "Five minutes have passed!" -a Timer -t 5000 -u 2
