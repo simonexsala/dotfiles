d979f57288fd52484edd5265afadc521dd39859b
Simone Sala <39414823+simonexsala@users.noreply.github.com>
2022-10-07 12:54:18 +0200

    Create config.lua

---
+++ lvim/config.lua
@@ -0,0 +1,92 @@
+lvim.log.level = "warn"
+lvim.format_on_save = false
+lvim.colorscheme = "catppuccin"
+vim.g.catppuccin_flavour = "mocha"
+lvim.transparent_window = true
+
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
+--
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.insert_mode["jj"] = { '<ESC>', { noremap = true } }
+lvim.keys.normal_mode["S"] = ":%s//g<Left><Left>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  i = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+    ["<C-n>"] = actions.cycle_history_next,
+    ["<C-p>"] = actions.cycle_history_prev,
+  },
+  n = {
+    ["<C-j>"] = actions.move_selection_next,
+    ["<C-k>"] = actions.move_selection_previous,
+  },
+}
+
+-- Change theme settings
+-- lvim.builtin.theme.options.dim_inactive = true
+-- lvim.builtin.theme.options.style = "storm"
+
+-- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+--  lvim.builtin.which_key.mappings["t"] = {
+--   name = "+Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+-- }
+
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enable = true
+
+lvim.lsp.installer.setup.ensure_installed = {
+    "sumeko_lua",
+    "jsonls",
+}
+
+lvim.plugins = {
+  { "catppuccin/nvim" },
+  { "junegunn/goyo.vim" }
+}
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "*.md" },
+  command = "set nospell",
+})
+
+vim.api.nvim_create_autocmd("BufLeave", {
+  pattern = { "*.md" },
+  command = "set spell",
+})
