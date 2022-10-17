6a72e227e4fbcef2f42f76056ea2455a8a9b6899
Simone Sala <39414823+simonexsala@users.noreply.github.com>
2022-10-17 22:08:57 +0200

    Update system-bluetooth-bluetoothctl.sh

--- polybar/system-bluetooth-bluetoothctl.sh
+++ polybar/system-bluetooth-bluetoothctl.sh
@@ -1,59 +1,78 @@
 #!/bin/sh
+
 bluetooth_print() {
-    bluetoothctl | while read -r; do
-        if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
+  bluetoothctl | while read -r; do
+    if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
 
-            devices_paired=$(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
-            counter=0
+      devices_paired=$(bluetoothctl devices | grep Device | cut -d ' ' -f 2)
+      counter=0
+      low_battery=false
+      notification_sent=false
 
-            for device in $devices_paired; do
-                device_info=$(bluetoothctl info "$device")
+      for device in $devices_paired; do
+        device_info=$(bluetoothctl info "$device")
 
-                if echo "$device_info" | grep -q "Connected: yes"; then
-                    device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
+        if echo "$device_info" | grep -q "Connected: yes"; then
+          device_alias=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
+          device_battery=$(upower --dump | grep "percentage" | head -n 1 | cut -d ' ' -f 15)
 
-                    if [ $counter -gt 0 ]; then
-                        printf ", %s" "$device_alias"
-                    else
-                        printf "%s" "$device_alias"
-                    fi
+          if ! $notification_sent ; then
+            if [ ${device_battery::-1} -lt 20 ]; then
+              low_battery=true
+            fi
+          fi
 
-                    counter=$((counter + 1))
-                fi
-            done
-            if [ $counter -eq 0 ]; then
-              printf "Inactive"
-              fi
+          if $low_battery ; then
+            notification_sent=1
+            low_battery_notify "$device_alias"
+          fi
+
+          if [ $counter -gt 0 ]; then
+            printf ", %s" "$device_alias"
+          else
+            printf "%s" "$device_alias"
+          fi
+
+          counter=$((counter + 1))
         fi
-    printf '\n'
-    done
+      done
+      if [ $counter -eq 0 ]; then
+        printf "Inactive"
+      fi
+    fi
+  printf '\n'
+  done
+}
+
+low_battery_notify() {
+  notify-send "$1" "Battery running low"
 }
 
 bluetooth_toggle() {
-    if bluetoothctl show | grep -q "Powered: no"; then
-        bluetoothctl power on >> /dev/null
-        sleep 1
+  if bluetoothctl show | grep -q "Powered: no"; then
+    bluetoothctl power on >> /dev/null
+    sleep 1
 
-        devices_paired = $(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
-        echo "$devices_paired" | while read -r line; do
-            bluetoothctl connect "$line" >> /dev/null
-        done
-    else
-        devices_paired = $(bluetoothctl paired-devices | grep Device | cut -d ' ' -f 2)
-        echo "$devices_paired" | while read -r line; do
-            bluetoothctl disconnect "$line" >> /dev/null
-        done
-        
-        bluetoothctl power off >> /dev/null
-        printf " \n"
-    fi
+    devices_paired = $(bluetoothctl devices | grep Device | cut -d ' ' -f 2)
+    echo "$devices_paired" | while read -r line; do
+      bluetoothctl connect "$line" >> /dev/null
+    done
+  else
+    devices_paired = $(bluetoothctl devices | grep Device | cut -d ' ' -f 2)
+    echo "$devices_paired" | while read -r line; do
+      bluetoothctl disconnect "$line" >> /dev/null
+    done
+    
+    bluetoothctl power off >> /dev/null
+    printf " \n"
+  fi
 }
 
 case "$1" in
-    --toggle)
-        bluetooth_toggle
-        ;;
-    *)
-        bluetooth_print
-        ;;
+  --toggle)
+    bluetooth_toggle
+    ;;
+  *)
+    bluetooth_print
+    ;;
 esac
