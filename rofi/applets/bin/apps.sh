3dbf31409a552e70dd5eebadbc936ddd2c1dd286
Simone Sala <39414823+simonexsala@users.noreply.github.com>
2022-10-19 18:50:52 +0200

    Add files via upload

---
+++ rofi/applets/bin/apps.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+theme="~/.config/rofi/applets/style.rasi"
+
+prompt='Applications'
+mesg="Pacman Packages: `pacman -Q | wc -l`" 
+
+term_cmd='alacritty'
+web_cmd='firefox'
+file_cmd='thunar'
+text_cmd='lvim'
+music_cmd='alacritty -e ncmpcpp'
+setting_cmd='alacritty -e lvim /home/simone/.config/polybar/config.ini'
+
+option_1=" Terminal"
+option_2=" File Browser" 
+option_3=" Text Editor"
+option_4=" Web Browser"
+option_5=" Music Player"
+option_6=" Polybar Settings"
+
+rofi_cmd() {
+	rofi -theme-str "listview {columns: 1; lines: 6;}" \
+		-theme-str 'textbox-prompt-colon {str: "";}' \
+		-dmenu \
+		-p "$prompt" \
+		-mesg "$mesg" \
+		-markup-rows \
+		-theme ${theme}
+}
+
+run_rofi() {
+	echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6" | rofi_cmd
+}
+
+run_cmd() {
+	if [[ "$1" == '--opt1' ]]; then
+		${term_cmd}
+	elif [[ "$1" == '--opt2' ]]; then
+		${file_cmd}
+	elif [[ "$1" == '--opt3' ]]; then
+		${text_cmd}
+	elif [[ "$1" == '--opt4' ]]; then
+		${web_cmd}
+	elif [[ "$1" == '--opt5' ]]; then
+		${music_cmd}
+	elif [[ "$1" == '--opt6' ]]; then
+		${setting_cmd}
+	fi
+}
+
+chosen="$(run_rofi)"
+case ${chosen} in
+    $option_1)
+		run_cmd --opt1
+        ;;
+    $option_2)
+		run_cmd --opt2
+        ;;
+    $option_3)
+		run_cmd --opt3
+        ;;
+    $option_4)
+		run_cmd --opt4
+        ;;
+    $option_5)
+		run_cmd --opt5
+        ;;
+    $option_6)
+		run_cmd --opt6
+        ;;
+esac
---
+++ rofi/powermenu/powermenu.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+dir="$HOME/.config/rofi/powermenu"
+theme='style'
+
+uptime=$(uptime -p)
+hours=${uptime:3:7}
+
+if [ "$hours" -eq "0 hours" ]; then
+  hours=${uptime:12}
+fi
+
+host=$(hostname)
+
+shutdown=' Shutdown'
+reboot=' Reboot'
+lock=' Lock'
+suspend=' Suspend'
+logout=' Logout'
+yes='Yes'
+no='No'
+
+rofi_cmd() {
+	rofi -dmenu \
+		-p "$host" \
+		-mesg "<b>Uptime: $hours</b>" \
+		-theme ${dir}/${theme}.rasi
+}
+
+confirm_cmd() {
+	rofi -theme-str 'window {location: north east; anchor: north east; fullscreen: false; width: 250px;}' \
+		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
+		-theme-str 'listview {columns: 2; lines: 1;}' \
+		-theme-str 'element-text {horizontal-align: 0.5;}' \
+		-theme-str 'textbox {horizontal-align: 0.5;}' \
+		-dmenu \
+		-p 'Confirmation' \
+		-mesg 'Are you Sure?' \
+		-theme ${dir}/${theme}.rasi
+}
+
+confirm_exit() {
+	echo -e "$yes\n$no" | confirm_cmd
+}
+
+run_rofi() {
+	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
+}
+
+run_cmd() {
+	selected="$(confirm_exit)"
+	if [[ "$selected" == "$yes" ]]; then
+		if [[ $1 == '--shutdown' ]]; then
+			systemctl poweroff
+		elif [[ $1 == '--reboot' ]]; then
+			systemctl reboot
+		elif [[ $1 == '--suspend' ]]; then
+			mpc -q pause
+			amixer set Master mute
+			systemctl suspend
+		elif [[ $1 == '--logout' ]]; then
+			if [[ "$DESKTOP_SESSION" == 'i3' ]]; then
+				i3-msg exit
+			fi
+		fi
+	else
+		exit 0
+	fi
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+		run_cmd --shutdown
+        ;;
+    $reboot)
+		run_cmd --reboot
+        ;;
+    $lock)
+		if [[ -x '/usr/bin/betterlockscreen' ]]; then
+			betterlockscreen -l
+		elif [[ -x '/usr/bin/i3lock' ]]; then
+			i3lock
+		fi
+        ;;
+    $suspend)
+		run_cmd --suspend
+        ;;
+    $logout)
+		run_cmd --logout
+        ;;
+esac
