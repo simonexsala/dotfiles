101fbf3d5ff80e72f4b1c54da103bf78ae6ddf2b
Simone Sala <39414823+simonexsala@users.noreply.github.com>
2022-10-22 16:24:43 +0200

    Update weather.sh

--- polybar/weather.sh
+++ polybar/weather.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 weatherreport="${XDG_DATA_HOME:-$HOME/.local/share}/weather/weatherreport"
 
-is_online() {
-  ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && return 0 || return 1
-}
-
 get_forecast() {
-#  if [ is_online ] ; then
-    curl -sf "wttr.in/Milan?format=%C" > "$weatherreport"
-    printf "@" >> "$weatherreport"
-    curl -sf "wttr.in/Milan?format=%t" >> "$weatherreport"
-#  fi
+  curl -sf "wttr.in/Milan?format=%C" > "$weatherreport"
+  printf "@" >> "$weatherreport"
+  curl -sf "wttr.in/Milan?format=%t" >> "$weatherreport"
+  printf "@" >> "$weatherreport"
+  curl -sf "wttr.in/Milan?format=%l" >> "$weatherreport"
+  printf "@" >> "$weatherreport"
+  curl -sf "wttr.in/Milan?format=%S" >> "$weatherreport"
+  printf "@" >> "$weatherreport"
+  curl -sf "wttr.in/Milan?format=%s" >> "$weatherreport"
+  printf "@" >> "$weatherreport"
+  curl -sf "wttr.in/Milan?format=%M" >> "$weatherreport"
+  printf "@" >> "$weatherreport"
+  curl -sf "wttr.in/Milan?format=%h" >> "$weatherreport"
 }
 
 get_data() {
@@ -21,8 +25,15 @@ get_data() {
   fi
 }
 
+check_error() {
+  error=$(cat "$weatherreport" | grep "Unknown location")
+
+  if [[ ! -z "$error" ]]; then
+    > $weatherreport
+  fi
+}
+
 print_weather() {
-  display=""
   now=$(date +"%k%M")
 
   case $weather in
@@ -30,7 +41,7 @@ print_weather() {
       night_or_day $now
       ;;
     *Rain* | *rain* | *showers* | *Sleet* | *sleet*)
-      display+="%{F#04a5e5}歹"
+      display+="%{F#04a5e5}煮"
       ;;
     *Snow* | *snow*)
       display+="%{F#89dceb}ﰕ" 
@@ -38,17 +49,17 @@ print_weather() {
     *Thunder* | *thunder* | *Wind* | *wind*)
       display+="%{F#74c7ec}煮"
       ;;
-    *Fog* | *fog*)
-      display+="%{F#9399b2}敖"
+    *Fog* | *fog* | *Mist* | *mist*)
+      display+="%{F#9399b2}"
       ;;
     *Cloud* | *cloud*)
-      display+="%{F#04a5e5}" 
+      display+="%{F#04a5e5}" 
       ;;
     "Partly cloudy" | Overcast)
       display+="%{F#04a5e5}杖" 
       ;;
     *)
-      display+="%{F#e78284}" 
+      display+="%{F#e78284} %{F#eff1f5}N/A"
       ;;
   esac
 
@@ -57,17 +68,15 @@ print_weather() {
 }
 
 night_or_day() {
-    tempTime=$1
-    if [ $tempTime -gt 700 -a $tempTime -lt 2100 ]; then 
-      display+="%{F#eed49f}滛"
-    else
-      display+="%{F#f2d5cf}望"
-    fi
+  tempTime=$1
+  if [ $tempTime -gt 700 -a $tempTime -lt 2100 ]; then 
+    display+="%{F#eed49f}滛"
+  else
+    display+="%{F#f2d5cf}望"
+  fi
 }
 
-# if [ "$(stat -c %y "$weatherreport" 2>/dev/null | cut -d' ' -f 2 | cut -d'.' -f 1)" -lt "$(date -d '20 minutes ago' '+%T')" ] ; then
-#  get_forecast
-# fi
 get_forecast
 get_data
+check_error
 print_weather
