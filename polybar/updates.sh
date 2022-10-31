47bf88c185c98d1239e8edd593c3693f9c8337d2
Simone Sala <39414823+simonexsala@users.noreply.github.com>
2022-10-31 16:54:55 +0100

    Update updates.sh

--- polybar/updates.sh
+++ polybar/updates.sh
@@ -1,4 +1,6 @@
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+updates="$HOME/.local/share/updates/updates"          
+
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l); then
   updates_arch=0
 fi
 
@@ -6,9 +8,15 @@ if ! updates_aur=$(paru -Qum 2> /dev/null | wc -l); then
   updates_aur=0
 fi
 
-updates=$((updates_arch + updates_aur))
-if [ "$updates" -gt 0 ]; then
-  echo "%{F#40a02b} %{F#eff1f5}$updates"
+echo "$updates_arch" > $updates
+echo "$updates_aur" >> $updates
+
+if [ "$updates_arch" -gt 0 ]; then
+  echo "%{F#40a02b} %{F#eff1f5}$updates_arch"
+elif [ "$updates_aur" -gt 0 ]; then
+  echo "%{F#40a02b} %{F#eff1f5}$updates_aur"
 else
+  # echo "%{F#a6d189} %{F#eff1f5}0"
   echo ""
 fi
+
